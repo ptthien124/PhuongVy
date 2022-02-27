0cdf3393ec3eec3e3b747c54e525f004ac07b053
ptthien124 <ptthien124@gmail.com>
2022-02-27 10:28:25 +0700

    update

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -41,25 +41,46 @@ function Parallax() {
   return (
     <div className="parallax">
       <div className="parallax__layer parallax__layer__0" style={styles0}>
-        <img src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_0.png?raw=true" />
+        <img
+          alt="bg-imgae"
+          src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_0.png?raw=true"
+        />
       </div>
       <div className="parallax__layer parallax__layer__1" style={styles1}>
-        <img src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_1.png?raw=true" />
+        <img
+          alt="bg-imgae"
+          src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_1.png?raw=true"
+        />
       </div>
       <div className="parallax__layer parallax__layer__2" style={styles2}>
-        <img src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_2.png?raw=true" />
+        <img
+          alt="bg-imgae"
+          src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_2.png?raw=true"
+        />
       </div>
       <div className="parallax__layer parallax__layer__3" style={styles3}>
-        <img src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_3.png?raw=true" />
+        <img
+          alt="bg-imgae"
+          src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_3.png?raw=true"
+        />
       </div>
       <div className="parallax__layer parallax__layer__4" style={styles4}>
-        <img src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_4.png?raw=true" />
+        <img
+          alt="bg-imgae"
+          src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_4.png?raw=true"
+        />
       </div>
       <div className="parallax__layer parallax__layer__5" style={styles5}>
-        <img src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_5.png?raw=true" />
+        <img
+          alt="bg-imgae"
+          src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_5.png?raw=true"
+        />
       </div>
       <div className="parallax__layer parallax__layer__6" style={styles6}>
-        <img src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_6.png?raw=true" />
+        <img
+          alt="bg-imgae"
+          src="https://github.com/samdbeckham/blog/blob/master/dev/_assets/images/articles/firewatch/layer_6.png?raw=true"
+        />
       </div>
       <div className="parallax__cover">
         <span>Sup bish!</span>
